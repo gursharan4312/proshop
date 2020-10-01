cd091db9f402da5c7350a93df050014d3bcdf46d
Gursharan Singh <gursharansingh4312@gmail.com>
2020-10-01 14:42:55 -0700

    database schemas created

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import products from "./data/products.js";
 const app = express();
-const products = require("./data/products");
-const dotenv = require("dotenv");
-
 dotenv.config();
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is running");
@@ -20,5 +22,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
