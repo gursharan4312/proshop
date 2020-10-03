ea23004639f7c6f0c640ff1614dfaaebc6b8683e
Gursharan Singh <gursharansingh4312@gmail.com>
2020-10-02 19:59:21 -0700

    redux added

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,19 @@
+import {PRODUCT_LIST_REQUEST,PRODUCT_LIST_SUCCESS,PRODUCT_LIST_FAIL} from '../constants/productConstants'
+import axios from axios
+
+export const listProducts = ()=>async (dispatch)=> {
+    try{
+        dispatch({type:PRODUCT_LIST_REQUEST})
+    
+        const {data} = await axios.get('/api/products')
+        dispatch({
+            type:PRODUCT_LIST_SUCCESS,
+            payload: data
+        })
+    }catch(err){
+        dispatch({
+            type: PRODUCT_LIST_FAIL,
+            payload: err.response && err.response.data.messgae ? err.response.data.messgae : err.messgae
+        })
+    }
+}
