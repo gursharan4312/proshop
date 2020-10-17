d24a9a3ef35ee545c03da3e43fd5f921f8971a2d
Gursharan Singh <gursharansingh4312@gmail.com>
2020-10-17 06:38:50 -0700

    file issue changed

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontent/build")));
+  app.use(express.static(path.join(__dirname, "frontent", "build")));
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
