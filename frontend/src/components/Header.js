485142c4ec0e02dd6826a8619332e865361b87f4
Gursharan Singh <gursharansingh4312@gmail.com>
2020-10-17 04:34:00 -0700

    search functionality added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -24,10 +24,8 @@ const Header = ({ history }) => {
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
+            <Route render={({ history }) => <SearchBox history={history} />} />
             <Nav className="ml-auto">
-              <Route
-                render={({ history }) => <SearchBox history={history} />}
-              />
               <LinkContainer to="/cart">
                 <Nav.Link>
                   <i className="fas fa-shopping-cart" /> Cart
