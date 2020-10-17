24a272e763300b3d1a7ff5c15586e788ddc9334e
Gursharan Singh <gursharansingh4312@gmail.com>
2020-10-17 08:14:15 -0700

    bug fix

--- server.js
+++ server.js
@@ -32,9 +32,10 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontent/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
+
+  // app.get("/*", (req, res) =>
+  //   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  // );
 } else {
   app.get("/", (req, res) => {
     res.send("API is running");
