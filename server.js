65322194b144ebecade2053f859cac6714dfe6e0
Gursharan Singh <gursharansingh4312@gmail.com>
2020-10-17 17:37:14 -0700

    bug fix

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontent/build"));
+  app.use(express.static(path.resolve(__dirname, "frontend", "build")));
 
   app.get("/*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
