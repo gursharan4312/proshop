841f02959c35470d3c8e2ed0985ebc820a89c574
Gursharan Singh <gursharansingh4312@gmail.com>
2020-12-29 17:09:13 -0800

    updated node-sass

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 const app = express();
 dotenv.config();
 connectDB();
+
 app.use(express.json());
 
 if (process.env.NODE_ENV === "development") {
