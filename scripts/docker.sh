dc21c260309fa8c71e15b45225d73831e6168625
Gursharan Singh <gursharansingh4312@gmail.com>
2020-12-30 14:34:52 -0800

    docker file included

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,5 +2,5 @@
 docker run  --network projects-network --name proshop --rm $1 -it \
         -v $PWD:/mnt/app \
         -v ~/env/proshop:/mnt/env \
-        -p 8080:3000 \
+        -p 8080:5000 \
         -w /mnt/app node:alpine cp /mnt/env/.env . && npm run start
