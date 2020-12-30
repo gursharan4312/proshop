ac2c3cc877af73baee80889e1efda0d4b416661c
Gursharan Singh <gursharansingh4312@gmail.com>
2020-12-29 19:24:14 -0800

    env added

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker run  --network projects-network --name proshop --rm $1 -it \
         -v $PWD:/mnt/app \
+        -v ~/env/proshop:/mnt/env \
         -p 8080:3000 \
-        -w /mnt/app node:alpine npm run production
+        -w /mnt/app node:alpine cp /mnt/env/.env . && npm run start
